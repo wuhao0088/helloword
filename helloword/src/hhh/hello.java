705bca7ab034654e2a22783b28e863e3d7c2cec0
wuhao <2452618635@qq.com>
2018-10-11 16:21:35 +0800

    kk

--- helloword/src/hhh/hello.java
+++ helloword/src/hhh/hello.java
@@ -3,6 +3,9 @@ package hhh;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("wqqq");
-	System.out.println("wqqq");
+	System.out.println("w");
+	System.out.println("wps");
+	
+	
 }
 }
